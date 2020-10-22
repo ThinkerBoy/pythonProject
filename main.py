93beee13d76c441d68fd05d7be17c293c136fc1a
黄涛 <huangtao@MacBook-2.local>
2020-10-22 16:42:26 +0800

    增加一行

--- main.py
+++ main.py
@@ -7,10 +7,11 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print("never")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm hh')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
